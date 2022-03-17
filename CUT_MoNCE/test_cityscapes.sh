a8e9529a47d47b604a4e7896afbba1116c849987
Fangneng Zhan <fzhan@mpi-inf.mpg.de>
2022-03-18 02:28:14 +0800

    Create test_cityscapes.sh

---
+++ CUT_MoNCE/test_cityscapes.sh
@@ -0,0 +1,8 @@
+python3 test.py \
+--dataroot /mnt/lustre/fnzhan/datasets/CVPR2022/cityscapes \
+--name cityscapes_inverse0.5_p1_n3 \
+--CUT_mode CUT \
+--phase test \
+--batch_size 12 \
+--gpu_ids '0' \
+--num_test 10000 \
