5272ef3bdef4905a7ba1aef96a7f92b2a5072be6
Fangneng Zhan <fzhan@mpi-inf.mpg.de>
2022-10-06 23:25:09 +0200

    Add files via upload

--- SPADE_MoNCE/data/pix2pix_dataset.py
+++ SPADE_MoNCE/data/pix2pix_dataset.py
@@ -19,16 +19,14 @@ class Pix2pixDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
 
-        label_paths, image_paths, instance_paths = self.get_paths(opt)
+        label_paths, image_paths = self.get_paths(opt)
 
-        util.natural_sort(label_paths)
-        util.natural_sort(image_paths)
-        if not opt.no_instance:
-            util.natural_sort(instance_paths)
+        if opt.dataset_mode != 'deepfashion':
+            util.natural_sort(label_paths)
+            util.natural_sort(image_paths)
 
         label_paths = label_paths[:opt.max_dataset_size]
         image_paths = image_paths[:opt.max_dataset_size]
-        instance_paths = instance_paths[:opt.max_dataset_size]
 
         if not opt.no_pairing_check:
             for path1, path2 in zip(label_paths, image_paths):
@@ -37,7 +35,6 @@ class Pix2pixDataset(BaseDataset):
 
         self.label_paths = label_paths
         self.image_paths = image_paths
-        self.instance_paths = instance_paths
 
         size = len(self.label_paths)
         self.dataset_size = size
@@ -45,46 +42,38 @@ class Pix2pixDataset(BaseDataset):
     def get_paths(self, opt):
         label_paths = []
         image_paths = []
-        instance_paths = []
         assert False, "A subclass of Pix2pixDataset must override self.get_paths(self, opt)"
-        return label_paths, image_paths, instance_paths
+        return label_paths, image_paths
 
     def paths_match(self, path1, path2):
         filename1_without_ext = os.path.splitext(os.path.basename(path1))[0]
         filename2_without_ext = os.path.splitext(os.path.basename(path2))[0]
         return filename1_without_ext == filename2_without_ext
 
-    def __getitem__(self, index):
-        # Label Image
-        label_path = self.label_paths[index]
-        label = Image.open(label_path)
+    def get_label_tensor(self, path):
+        label = Image.open(path)
         params = get_params(self.opt, label.size)
         transform_label = get_transform(self.opt, params, method=Image.NEAREST, normalize=False)
         label_tensor = transform_label(label) * 255.0
         label_tensor[label_tensor == 255] = self.opt.label_nc  # 'unknown' is opt.label_nc
+        return label_tensor, params
+
+    def __getitem__(self, index):
+        # Label Image
+        label_path = self.label_paths[index]
+        label_tensor, params = self.get_label_tensor(label_path)
 
         # input image (real images)
         image_path = self.image_paths[index]
-        assert self.paths_match(label_path, image_path), \
-            "The label_path %s and image_path %s don't match." % \
-            (label_path, image_path)
+        # assert self.paths_match(label_path, image_path), \
+        #     "The label_path %s and image_path %s don't match." % \
+        #     (label_path, image_path)
         image = Image.open(image_path)
         image = image.convert('RGB')
 
         transform_image = get_transform(self.opt, params)
         image_tensor = transform_image(image)
-
-        # if using instance maps
-        if self.opt.no_instance:
-            instance_tensor = 0
-        else:
-            instance_path = self.instance_paths[index]
-            instance = Image.open(instance_path)
-            if instance.mode == 'L':
-                instance_tensor = transform_label(instance) * 255
-                instance_tensor = instance_tensor.long()
-            else:
-                instance_tensor = transform_label(instance)
+        instance_tensor = 0
 
         input_dict = {'label': label_tensor,
                       'instance': instance_tensor,
